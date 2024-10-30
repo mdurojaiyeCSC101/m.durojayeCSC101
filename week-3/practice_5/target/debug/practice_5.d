fa41fa450e3c999f025ef99c2bab38823abb3f38
MO <185250522+mdurojaiyeCSC101@users.noreply.github.com>
2024-10-30 18:34:29 +0100

    (week 3)

---
+++ week-3/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\msiki\OneDrive\Documents\m.durojayeCSC101\week-3\practice_5\target\debug\practice_5.exe: C:\Users\msiki\OneDrive\Documents\m.durojayeCSC101\week-3\practice_5\src\main.rs
---
+++ week-3/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\msiki\OneDrive\Documents\m.durojayeCSC101\week-3\practice_7\target\debug\practice_7.exe: C:\Users\msiki\OneDrive\Documents\m.durojayeCSC101\week-3\practice_7\src\main.rs
